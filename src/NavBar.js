0c9a2f6be353aac38dc054e9c42dd25e6743c30d
AlexKynoch <67015973+AlexKynoch@users.noreply.github.com>
2021-09-24 15:14:11 +0100

    navbar layout

--- src/NavBar.js
+++ src/NavBar.js
@@ -3,29 +3,51 @@ import Navbar from 'react-bootstrap/Navbar'
 import Container from 'react-bootstrap/Container'
 import Nav from 'react-bootstrap/Nav'
 import './NavBar.css'
+import navImg from "./images/logo.png"
 
 
 const NavBar = () => {
+
+  function logout() {
+    console.log("im the logout function")
+  }
   return (
     <>
- <Navbar collapseOnSelect expand="sm" bg="primary" variant="light">
-  <Container>
-   <Navbar.Brand>
-          <a class="navbar.brand" href="#"><img src="logo.svg"></img></a>
-    
-    </Navbar.Brand>
-  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-  <Navbar.Collapse id="responsive-navbar-nav">
 
-    <Nav>
-      <Nav.Link href="#deets">Calender</Nav.Link>
-      <Nav.Link href="#deets">Profile</Nav.Link>   
-    </Nav>
-  </Navbar.Collapse>
-  </Container>
-</Navbar>
+      <Navbar>
+        <Container>
+
+          <Navbar.Brand href="/">
+            <img src={navImg}></img>
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+
+
+
+            <Nav className="justify-content-end me-auto" activeKey="/home" style={{ width: "100%" }}>
+              {/* <Nav.Item>
+              <Nav.Link href="/home">Active</Nav.Link>
+            </Nav.Item> */}
+              <Nav.Item>
+                <Nav.Link eventKey="link-1">Calender</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link eventKey="link-2">Profile</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link onClick={logout}>Log Out
+                </Nav.Link>
+              </Nav.Item>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+
+
+
     </>
   );
 };
-  
+
 export default NavBar;
