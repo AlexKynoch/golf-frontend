775c44b170a828aa446fa613d14a1649efc924b2
AlexKynoch <67015973+AlexKynoch@users.noreply.github.com>
2021-09-27 17:00:11 +0100

    form populated with user data

--- src/Profile.js
+++ src/Profile.js
@@ -1,35 +1,86 @@
-import React from 'react';
+
 // import Navbar from 'react-bootstrap/Navbar'
 import Card from 'react-bootstrap/Card'
 import Button from 'react-bootstrap/Button'
 import './Profile.css'
 import ProfileForm from './ProfileForm'
+import React, { useState, useEffect } from "react"
+
 
 
 function Profile() {
+
+
+
+    const [users, cUsers] = useState(
+        {
+            username: "Jonny5",
+            firstName: "Johnathan",
+            surname: "Smith",
+            location: "Sheffield",
+            email: "jojosmith@hotmail.com",
+            phone: "07778574321"
+        }
+    )
+
+    // userInfo = [...users];
+    console.log(users)
+
     return (
-        <div class="container" >
+        <div className="container" >
             <Card id="myProfile" style={{ width: '30rem' }}>
                 <Card.Body>
                     <Card.Title>My Profile</Card.Title>
                     <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
-                    <ProfileForm />
+
+                    <form>
+                        <div className="form-group row">
+                            <label form="inputUsername3" className="col-sm-3 col-form-label">Username</label>
+                            <div className="col-sm-8">
+                                <input type="username" className="form-control" id="inputUsername3" value={users.username}></input>
+                            </div>
+                        </div>
+                        <div className="form-group row">
+                            <label form="inputFirstName3" className="col-sm-3 col-form-label">First Name</label>
+                            <div className="col-sm-8">
+                                <input type="firstname" className="form-control" id="inputFirstName3" value={users.firstName}></input>
+                            </div>
+                        </div>
+                        <div className="form-group row">
+                            <label form="inputSurname3" className="col-sm-3 col-form-label">Surname</label>
+                            <div className="col-sm-8">
+                                <input type="surname" className="form-control" id="inputSurname3" value={users.surname}></input>
+                            </div>
+                        </div>
+                        <div className="form-group row">
+                            <label form="inputLocation3" className="col-sm-3 col-form-label">Location</label>
+                            <div className="col-sm-8">
+                                <input type="location" className="form-control" id="inputLocation3" value={users.location}></input>
+                            </div>
+                        </div>
+                        <div className="form-group row">
+                            <label form="inputEmail3" className="col-sm-3 col-form-label">Email</label>
+                            <div className="col-sm-8">
+                                <input type="email" className="form-control" id="inputEmail3" value={users.email}></input>
+                            </div>
+                        </div>
+                        <div className="form-group row">
+                            <label form="inputPhone3" className="col-sm-3 col-form-label">Phone</label>
+                            <div className="col-sm-8">
+                                <input type="phone" className="form-control" id="inputPhone3" value={users.phone}></input>
+                            </div>
+                        </div>
+
+
+                    </form >
+
                     <div className="btn-container justify-content-end">
                         <Button variant="light">Update</Button>
                     </div>
                 </Card.Body>
             </Card>
 
-            <Card id="volunteer" style={{ width: '30rem' }}>
-                <Card.Body>
-                    <Card.Title >Volunteer</Card.Title>
-                    <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
-                    <ProfileForm />
-                    <div className="btn-container justify-content-end">
-                        <Button variant="light">Update</Button>
-                    </div>
-                </Card.Body>
-            </Card>
+
         </div >
     )
 }
--- src/ProfileForm.js
+++ src/ProfileForm.js
@@ -4,40 +4,40 @@ function ProfileForm() {
     return (
         <div>
             <form>
-                <div class="form-group row">
-                    <label for="inputUsername3" class="col-sm-3 col-form-label">Username</label>
-                    <div class="col-sm-8">
-                        <input type="username" class="form-control" id="inputUsername3" placeholder="Username"></input>
+                <div className="form-group row">
+                    <label form="inputUsername3" className="col-sm-3 col-form-label">Username</label>
+                    <div className="col-sm-8">
+                        <input type="username" className="form-control" id="inputUsername3" placeholder="Username"></input>
                     </div>
                 </div>
-                <div class="form-group row">
-                    <label for="inputFirstName3" class="col-sm-3 col-form-label">First Name</label>
-                    <div class="col-sm-8">
-                        <input type="firstname" class="form-control" id="inputFirstName3" placeholder="First Name"></input>
+                <div className="form-group row">
+                    <label form="inputFirstName3" className="col-sm-3 col-form-label">First Name</label>
+                    <div className="col-sm-8">
+                        <input type="firstname" className="form-control" id="inputFirstName3" placeholder="First Name"></input>
                     </div>
                 </div>
-                <div class="form-group row">
-                    <label for="inputSurname3" class="col-sm-3 col-form-label">Surname</label>
-                    <div class="col-sm-8">
-                        <input type="surname" class="form-control" id="inputSurname3" placeholder="Second Name"></input>
+                <div className="form-group row">
+                    <label form="inputSurname3" className="col-sm-3 col-form-label">Surname</label>
+                    <div className="col-sm-8">
+                        <input type="surname" className="form-control" id="inputSurname3" placeholder="Second Name"></input>
                     </div>
                 </div>
-                <div class="form-group row">
-                    <label for="inputLocation3" class="col-sm-3 col-form-label">Location</label>
-                    <div class="col-sm-8">
-                        <input type="location" class="form-control" id="inputLocation3" placeholder="Location"></input>
+                <div className="form-group row">
+                    <label form="inputLocation3" className="col-sm-3 col-form-label">Location</label>
+                    <div className="col-sm-8">
+                        <input type="location" className="form-control" id="inputLocation3" placeholder="Location"></input>
                     </div>
                 </div>
-                <div class="form-group row">
-                    <label for="inputEmail3" class="col-sm-3 col-form-label">Email</label>
-                    <div class="col-sm-8">
-                        <input type="email" class="form-control" id="inputEmail3" placeholder="Email"></input>
+                <div className="form-group row">
+                    <label form="inputEmail3" className="col-sm-3 col-form-label">Email</label>
+                    <div className="col-sm-8">
+                        <input type="email" className="form-control" id="inputEmail3" placeholder="Email"></input>
                     </div>
                 </div>
-                <div class="form-group row">
-                    <label for="inputPhone3" class="col-sm-3 col-form-label">Phone</label>
-                    <div class="col-sm-8">
-                        <input type="phone" class="form-control" id="inputPhone3" placeholder="Phone"></input>
+                <div className="form-group row">
+                    <label form="inputPhone3" className="col-sm-3 col-form-label">Phone</label>
+                    <div className="col-sm-8">
+                        <input type="phone" className="form-control" id="inputPhone3" placeholder="Phone"></input>
                     </div>
                 </div>
 
